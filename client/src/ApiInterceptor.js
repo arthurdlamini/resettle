85663ae694650a452d378ae26a7a9716bd7de5dc
Senzo Ncongwane <senzoncongwane21@gmail.com>
2024-08-07 14:06:15 +0200

    app -change

--- client/src/ApiInterceptor.js
+++ client/src/ApiInterceptor.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = (user) => {
-	const token = user.token; 
+	const token = user.token; //Update by masombu8ka
 	const instance = axios.create({
 		baseURL: 'http://localhost:5000/api/v1',
 		headers: {
